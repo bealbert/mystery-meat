dc1a12e82ce75c72f9cd2c2b0b9d3ca1f66e6728
B.E. Albert <btrebla23@gmail.com>
2024-11-14 23:23:04 -0500

    ok i THINK its working the way it should now

---
+++ mm-client/src/components/SlideshowComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="slideshow">
+        <button id="move-left"></button>
+        <div id="box">
+        </div>
+        <button id="move-right"></button>
+    </section>
+</template>
+
+<script>
+</script>
+
+<style>
+body {
+    background: #333;
+}
+
+#box {
+    background: white;
+    width: 200px;
+    height: 200px;
+    position: relative;
+    animation-name: img-slideshow;
+    animation-duration: 4s;
+}
+
+@keyframes img-slideshow {
+
+}
+</style>
