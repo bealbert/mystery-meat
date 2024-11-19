010eae35fda0847a18e74f2dfa7fa27bc0981c9a
B.E. Albert <btrebla23@gmail.com>
2024-11-18 19:19:08 -0500

    working on slideshow

--- mm-client/src/components/PortfolioMain.vue
+++ mm-client/src/components/PortfolioMain.vue
@@ -1,8 +1,19 @@
 <template>
-  <div></div>
+  <div>
+
+    <slideshow-component></slideshow-component>
+
+  </div>
 </template>
 
 <script>
+import SlideshowComponent from '../components/SlideshowComponent.vue';
+
+export default {
+  components: {
+    SlideshowComponent
+  }
+}
 </script>
 
 <style></style>
